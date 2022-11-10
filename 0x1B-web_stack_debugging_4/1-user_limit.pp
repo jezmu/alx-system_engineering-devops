b4d51b5df7c92a87154255bca510301029dc1f17
root <jezmuenterprises@gmail.com>
2022-11-10 08:59:46 -0800

    update

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,16 @@
+# Enabling the Holberton User to login and open files without error.
+# Author: Eng. Waython Yesse
+# Address: waythonny@yahoo.com
+# Year: 2022 May 13
+
+# Increasing the hard file limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increasing the soft file limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
